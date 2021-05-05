0f14e3b849457b2108428a490e1a30a7c6f4dd51
carrossoni <carrossoni@gmail.com>
2021-05-05 14:26:52 -0500

    Support for 7.1.4
    Support for 7.1.4

--- centosvmCDP.sh
+++ centosvmCDP.sh
@@ -60,7 +60,7 @@ echo "-- Install CM and MariaDB"
 
 # CM 7
 cd /
-wget https://archive.cloudera.com/cm7/7.1.3/redhat7/yum/cloudera-manager-trial.repo -P /etc/yum.repos.d/
+wget https://archive.cloudera.com/cm7/7.1.4/redhat7/yum/cloudera-manager-trial.repo -P /etc/yum.repos.d/
 
 # MariaDB 10.1
 cat - >/etc/yum.repos.d/MariaDB.repo <<EOF
@@ -151,14 +151,7 @@ EOF
 
 
 echo "-- Install CSDs"
-#wget https://archive.cloudera.com/CFM/csd/1.0.1.0/NIFI-1.9.0.1.0.1.0-12.jar -P /opt/cloudera/csd/
-#wget https://archive.cloudera.com/CFM/csd/1.0.1.0/NIFICA-1.9.0.1.0.1.0-12.jar -P /opt/cloudera/csd/
-#wget https://archive.cloudera.com/CFM/csd/1.0.1.0/NIFIREGISTRY-0.3.0.1.0.1.0-12.jar -P /opt/cloudera/csd/
-# CDSW CSD: must update descriptors so it can install on CR7
-#wget https://archive.cloudera.com/cdsw1/1.6.1/csd/CLOUDERA_DATA_SCIENCE_WORKBENCH-CDH6-1.6.1.jar -P cdswjar
-#cd cdswjar/
-#mv CLOUDERA_DATA_SCIENCE_WORKBENCH-CDH6-1.6.1.jar ~
-#cd ..
+
 # install local CSDs
 mv ~/*.jar /opt/cloudera/csd/
 mv /home/centos/*.jar /opt/cloudera/csd/
@@ -170,27 +163,6 @@ mv ~/*.parcel ~/*.parcel.sha /opt/cloudera/parcel-repo/
 mv /home/centos/*.parcel /home/centos/*.parcel.sha /opt/cloudera/parcel-repo/
 chown cloudera-scm:cloudera-scm /opt/cloudera/parcel-repo/*
 
-#echo "-- Install CEM Tarballs"
-#mkdir -p /opt/cloudera/cem
-#wget https://archive.cloudera.com/CEM/centos7/1.x/updates/1.0.0.0/CEM-1.0.0.0-centos7-tars-tarball.tar.gz -P /opt/cloudera/cem
-#tar xzf /opt/cloudera/cem/CEM-1.0.0.0-centos7-tars-tarball.tar.gz -C /opt/cloudera/cem
-#tar xzf /opt/cloudera/cem/CEM/centos7/1.0.0.0-54/tars/efm/efm-1.0.0.1.0.0.0-54-bin.tar.gz -C /opt/cloudera/cem
-#tar xzf /opt/cloudera/cem/CEM/centos7/1.0.0.0-54/tars/minifi/minifi-0.6.0.1.0.0.0-54-bin.tar.gz -C /opt/cloudera/cem
-#tar xzf /opt/cloudera/cem/CEM/centos7/1.0.0.0-54/tars/minifi/minifi-toolkit-0.6.0.1.0.0.0-54-bin.tar.gz -C /opt/cloudera/cem
-#rm -f /opt/cloudera/cem/CEM-1.0.0.0-centos7-tars-tarball.tar.gz
-#ln -s /opt/cloudera/cem/efm-1.0.0.1.0.0.0-54 /opt/cloudera/cem/efm
-#ln -s /opt/cloudera/cem/minifi-0.6.0.1.0.0.0-54 /opt/cloudera/cem/minifi
-#ln -s /opt/cloudera/cem/efm/bin/efm.sh /etc/init.d/efm
-#chown -R root:root /opt/cloudera/cem/efm-1.0.0.1.0.0.0-54
-#chown -R root:root /opt/cloudera/cem/minifi-0.6.0.1.0.0.0-54
-#chown -R root:root /opt/cloudera/cem/minifi-toolkit-0.6.0.1.0.0.0-54
-#rm -f /opt/cloudera/cem/efm/conf/efm.properties
-#cp conf/efm.properties /opt/cloudera/cem/efm/conf
-#rm -f /opt/cloudera/cem/minifi/conf/bootstrap.conf
-#cp conf/bootstrap.conf /opt/cloudera/cem/minifi/conf
-#sed -i "s/YourHostname/`hostname -f`/g" /opt/cloudera/cem/efm/conf/efm.properties
-#sed -i "s/YourHostname/`hostname -f`/g" /opt/cloudera/cem/minifi/conf/bootstrap.conf
-#/opt/cloudera/cem/minifi/bin/minifi.sh install
 
 
 echo "-- Enable passwordless root login via rsa key"
